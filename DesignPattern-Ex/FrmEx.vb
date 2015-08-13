5098a95e4180e6b30eac25bbc02da24793bedd2c
Artit Kiuwilai <first087@gmail.com>
2015-08-13 16:30:46 +0700

    Normal way

--- DesignPattern-Ex/FrmEx.vb
+++ DesignPattern-Ex/FrmEx.vb
@@ -1,5 +1,7 @@
 ﻿Public Class FrmEx
 
+    Private pop As People
+
     Public Sub New()
         ' This call is required by the Windows Form Designer.
         InitializeComponent()
@@ -8,4 +10,56 @@
         LblFullName.Text = "-"
     End Sub
 
+    Private Sub FrmEx_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        pop = New People()
+    End Sub
+
+    Private Sub DtpDOB_ValueChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles DtpDOB.ValueChanged
+        pop.DOB = DtpDOB.Value
+    End Sub
+
+    Private Sub RdoGenderUnknown_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles RdoGenderUnknown.CheckedChanged
+        If RdoGenderUnknown.Checked Then pop.Gender = People.GenderEnum.UNKNOWN
+    End Sub
+
+    Private Sub RdoGenderMale_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles RdoGenderMale.CheckedChanged
+        If RdoGenderMale.Checked Then pop.Gender = People.GenderEnum.MALE
+    End Sub
+
+    Private Sub RdoGenderFemale_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles RdoGenderFemale.CheckedChanged
+        If RdoGenderFemale.Checked Then pop.Gender = People.GenderEnum.FEMALE
+    End Sub
+
+    Private Sub BtnQuery_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BtnQuery.Click
+        If Not pop.Query(1234567890123) Then Return
+
+        ShowPOP()
+        EnableControl()
+    End Sub
+
+    Private Sub BtnUpdate_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles BtnUpdate.Click
+        Dim popData As String = pop.Update()
+
+        MessageBox.Show(popData)
+    End Sub
+
+    Private Sub ShowPOP()
+        LblFullName.Text = pop.FullName
+        DtpDOB.Value = pop.DOB
+
+        Select Case pop.Gender
+            Case People.GenderEnum.UNKNOWN : RdoGenderUnknown.Checked = True
+            Case People.GenderEnum.MALE : RdoGenderMale.Checked = True
+            Case People.GenderEnum.FEMALE : RdoGenderFemale.Checked = True
+        End Select
+    End Sub
+
+    Private Sub EnableControl()
+        LblFullName.Enabled = True
+        DtpDOB.Enabled = True
+        RdoGenderUnknown.Enabled = True
+        RdoGenderMale.Enabled = True
+        RdoGenderFemale.Enabled = True
+        BtnUpdate.Enabled = True
+    End Sub
 End Class
